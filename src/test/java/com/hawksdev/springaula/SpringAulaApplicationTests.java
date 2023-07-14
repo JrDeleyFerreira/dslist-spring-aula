426be4a06528159a210da996e246bfa4cc0d68e5
JrDeleyFerreira <deleyalemao@gmail.com>
2023-07-14 00:33:18 -0300

    Primeiro projeto Java Spring. Adicionando Entidade.

---
+++ src/test/java/com/hawksdev/springaula/SpringAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hawksdev.springaula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
