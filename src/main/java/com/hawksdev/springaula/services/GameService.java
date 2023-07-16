fbf2e05780dc0c826ce0464ccf7ac85eb8be618f
JrDeleyFerreira <deleyalemao@gmail.com>
2023-07-16 15:34:52 -0300

    Modelos de domínio, mapeamento n pra n e Seed da base.

--- src/main/java/com/hawksdev/springaula/services/GameService.java
+++ src/main/java/com/hawksdev/springaula/services/GameService.java
@@ -2,11 +2,12 @@ package com.hawksdev.springaula.services;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import com.hawksdev.springaula.dto.GameMinDto;
 import com.hawksdev.springaula.repositories.IGameRepository;
 
-@Component // Ou @Service - Injeção de dependência
+@Service // Ou @Component - Injeção de dependência
 public class GameService {
 
   @Autowired // Injeção
